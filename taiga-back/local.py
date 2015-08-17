ad954da2d2e96710d0f8a33f3e9e9c39c28743ca
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-08-17 20:45:53 +0100

    Enable gogs and letschat plugins

--- taiga-back/local.py
+++ taiga-back/local.py
@@ -2,6 +2,9 @@ from .common import *
 
 from .dockerenv import *
 
+INSTALLED_APPS += ["taiga_contrib_gogs","taiga_contrib_letschat"]
+PROJECT_MODULES_CONFIGURATORS["gogs"] = "taiga_contrib_gogs.services.get_or_generate_config"
+
 # THROTTLING
 #REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
 #    "anon": "20/min",
